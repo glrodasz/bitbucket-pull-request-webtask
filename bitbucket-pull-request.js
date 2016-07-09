8999926c1ee049f9c03f881a5a79f4539c0fc6f6
Guillermo Rodas <garethderioth@gmail.com>
2016-07-09 15:37:42 -0500

    Fix branch names

--- bitbucket-pull-request.js
+++ bitbucket-pull-request.js
@@ -1,11 +1,11 @@
-"use lastest";
+"use latest";
 
 module.exports = function ({ data: { actor, pullrequest, repository } }, done) {
   const { username, display_name } = actor;
   const { title, description, source, destination } = pullrequest;
   const { links: { self, html } } = repository;
 
-  console.log(`${display_name} (${username}), has opened the following pull request **${title}** that says \"${description}\" from ${source.branch} to ${destination.branch}`);
+  console.log(`${display_name} (${username}), has opened the following pull request **${title}** that says \"${description}\" from ${source.branch.name} to ${destination.branch.name}`);
   console.log(self, html);
 
   done(null, 'Done');
