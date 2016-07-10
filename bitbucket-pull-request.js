e4a947c3b975d6b5deb803bfe78ee9716805a33c
Guillermo Rodas <garethderioth@gmail.com>
2016-07-09 22:38:35 -0500

    Improve readability

--- bitbucket-pull-request.js
+++ bitbucket-pull-request.js
@@ -1,12 +1,22 @@
 "use latest";
 
-module.exports = function ({ data: { actor, pullrequest, repository } }, done) {
-  const { username, display_name } = actor;
-  const { title, description, source, destination } = pullrequest;
-  const { links: { self, html } } = repository;
+module.exports = function ({ data }, done) {
+  const {
+    actor: { username, display_name },
+    pullrequest: { title, description, source, destination },
+    repository: { links: { self, html } }
+  } = data;
 
-  console.log(`${display_name} (${username}), has opened the following pull request **${title}** that says \"${description}\" from ${source.branch.name} to ${destination.branch.name}`);
-  console.log(self, html);
+  const getBranchName = branch => branch && branch.name ?
+    branch.name : '(no branch specified)';
+
+  let message = [];
+  message.push(`${display_name} (${username})`);
+  message.push(', has opened the following pull request');
+  message.push(`**${title}** that *${description}*`);
+  message.push(`from ${getBranchName(source)} to ${getBranchName(destination)}`);
+
+  console.log(message.join(' '), self, html);
 
   done(null, 'Done');
 }
