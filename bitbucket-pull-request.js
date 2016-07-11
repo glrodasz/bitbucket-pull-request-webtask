57905f0196b9a482974279abdb0d5842bd69ca1a
Guillermo Rodas <garethderioth@gmail.com>
2016-07-11 02:15:17 -0500

    Documentation

--- bitbucket-pull-request.js
+++ bitbucket-pull-request.js
@@ -3,11 +3,26 @@
 const sendgrid = require('sendgrid'); // eslint-disable-line
 const marked = require('marked'); // eslint-disable-line
 
+/**
+ * Get the branch name based on the source
+ * @param {object} source - Source from Bitbucket payload
+ * @return {string} branch name
+ */
 function getBranchName(source) {
   return source && source.branch && source.branch.name ?
     source.branch.name : '(no branch specified)';
 }
 
+/**
+ * Send email using Sendgrid
+ * @param {string} key - Sendgrid API Key
+ * @param {string} emailTo - Email to send
+ * @param {string} emailFrom - Email from send
+ * @param {string} subject - Subject of the message
+ * @param {string} message - HTML string of the message
+ * @param {Function} cb - Callback for sucess response
+ * @return {void}
+ */
 function sendEmail(key, emailTo, emailFrom, subject, message, cb) {
   sendgrid(key).send({
     subject,
@@ -22,6 +37,11 @@ function sendEmail(key, emailTo, emailFrom, subject, message, cb) {
   });
 }
 
+/**
+ * Build the message for the email
+ * @param {object} data - Data from Bitbucket payload
+ * @return {void}
+ */
 function buildMessage(data) {
   const {
     actor: { username, display_name },
