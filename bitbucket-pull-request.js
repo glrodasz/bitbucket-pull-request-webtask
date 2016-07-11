ac83e68ec63f62993e3d767ed0f9fb4d07e44e35
Guillermo Rodas <garethderioth@gmail.com>
2016-07-11 01:12:49 -0500

    Linter added

--- bitbucket-pull-request.js
+++ bitbucket-pull-request.js
@@ -1,43 +1,46 @@
-"use latest";
+'use latest';
 
-const sendgrid = require('sendgrid');
-const marked = require('marked');
+const sendgrid = require('sendgrid'); // eslint-disable-line
+const marked = require('marked'); // eslint-disable-line
 
-function sendEmail(key, emailTo, emailFrom, subject, message) {
+function getBranchName(source) {
+  return source && source.branch && source.branch.name ?
+    source.branch : '(no branch specified)';
+}
+
+function sendEmail(key, emailTo, emailFrom, subject, message, cb) {
   sendgrid(key).send({
+    subject,
     to: emailTo,
     from: emailFrom,
-    subject: subject,
     html: message,
   }, (err, json) => {
     if (err) {
       return console.error(err);
     }
-    done(null, json);
+    return cb(null, json);
   });
 }
 
-module.exports = function ({ data }, done) {
+function buildMessage(data) {
   const {
     actor: { username, display_name },
-    pullrequest: {  title, source, destination },
-    repository: { links: { self, html } },
-    SENDGRID_KEY, EMAIL_TO, EMAIL_FROM
+    pullrequest: { title, source, destination },
   } = data;
 
-  // Check if the source branch has a name
-  const getBranchName = source => source && source.branch && source.branch.name ?
-    source.branch : '(no branch specified)';
-
-  const subject = `${title} - Bitbucket pull request`;
-
   // The message is pushed in an array for redability
-  let message = [];
-  message.push(`**${display_name}** *(${username})*:`);
+  const message = [];
+  message.push(`**${display_name}** *(${username})*:`); // eslint-disable-line
   message.push('has created the following pull request:');
   message.push(`**${title}**`);
   message.push(`from ${getBranchName(source)} to ${getBranchName(destination)}`);
 
-  // Send email via sendgrid
-  sendEmail(SENDGRID_KEY, EMAIL_TO, EMAIL_FROM, subject, marked(message.join(' ')));
+  return marked(message.join(' '));
 }
+
+module.exports = function webtask({ data }, done) {
+  const { pullrequest: { title }, SENDGRID_KEY, EMAIL_TO, EMAIL_FROM } = data;
+  const subject = `${title} - Bitbucket pull request`;
+
+  sendEmail(SENDGRID_KEY, EMAIL_TO, EMAIL_FROM, subject, buildMessage(data), done);
+};
